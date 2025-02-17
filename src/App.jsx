0e459911544bd150d5f73920b5f3a6b7639bb3d5
subham777546 <subhampatnaik0111@gmail.com>
2025-02-17 12:21:06 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,196 @@
-// import React from 'react'
-// import Navbar from './components/Navbar';
-// import {BrowserRouter  , Routes , Route} from "react-router-dom";
-// import Dashboard from './pages/Dashboard';
-// import Clasroom from './pages/Clasroom';
-// import Assessments from './pages/Assessments';
-// import Store from './pages/Store';
-// import Calender from './pages/Calender';
-// import News from './pages/News';
-// import Blog from './pages/Blog';
-// import PageNotFound from './pages/PageNotFound';
-// import MyAccount from './pages/MyAccount';
-// import MySettings from './pages/MySettings';
+// function App() {
+//   // let ele = React.createElement(
+//   //   "div",
+//   //   { className: "App" },
+//   //   React.createElement("h1", null, "Header")
+//   // );
+//   let username = "Rohan";
+//   return (
+//     // <div className="App">
+//     //   <h1 style={{ color: "red" }}>Name: Subhapreet Patro</h1>
+//     //   <input />
+//     // </div>
+//     // <section>
+//     //   <h1>One JSX Element</h1>
+//     //   <p>lorem3</p>
+//     // </section>
+//     <div>
+//       <h1>{username}</h1>
+//       <p>{100 + 100}</p>
+//     </div>
+//   );
+// }
 
+// export default App;
+
+// ! Components
+// ? CBC
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Class based Component</h1>;
+//   }
+// }
+
+// export default App;
+
+// ? FBC
+// function App() {
+//   return <h1>Functional Based Component</h1>;
+// }
+// export default App;
+
+// OR
+
+// const App = () => {
+//   return <h1>Functional based Component</h1>;
+// };
+// export default App;
+
+// !Component composition
+// import React from "react";
+// import Navbar from "./components/Navbar";
+// import Main from "./components/Main";
+// import Sidebar1 from "./components/Sidebar1";
+// import Sidebar2 from "./components/Sidebar2";
+// import Footer from "./components/Footer";
 
 // const App = () => {
 //   return (
-//     <BrowserRouter>
-//         <Navbar/>
-//         <Routes>
-//             <Route path='/' element={<Dashboard/>}/>
-//             <Route path='/classroom' element={<Clasroom/>}/>
-//             <Route path='/assessment' element={<Assessments/>}/>
-//             <Route path='/store' element={<Store/>}/>
-//             <Route path='/calender' element={<Calender/>}/>
-//             <Route path='/news' element={<News/>}/>
-//             <Route path='/blog' element={<Blog/>}/>
-        
-//             <Route path='/profile' element={<profile/>}>
-                 
-//                 <Route path='/profile/myaccount' element={<MyAccount/>}/>
-//                <Route path='/profile/mysettings' element={<MySettings/>}/>
-//               </Route>          
-//               <Route path='*' element={<PageNotFound/>}/>
+//     <div className="app">
+//       <Navbar />
+//       <Main />
+//       <div className="sidebar">
+//         <Sidebar1 />
+//         <Sidebar2 />
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
 
-//         </Routes>
-//     </BrowserRouter>
-//   )
+// export default App;
+
+// ! props
+// import React, { Component } from "react";
+// import CBCPropEx from "./propsex/CBCPropEx";
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         App
+//         <CBCPropEx username="Subhapreet" />
+//       </div>
+//     );
+//   }
 // }
 
-// export default App
-import React from 'react'
-import {BrowserRouter , Routes , Route} from "react-router-dom";
-import Home from './crudcomponents/Home' ;
-import Create from './crudcomponents/Create';
-import Edit from './crudcomponents/Edit';
-const App = () => {
-return (
-<BrowserRouter>
-<Routes>
-<Route path='/' element={<Home/>}/>
-<Route path='/create' element={<Create/>}/>
-<Route path='/edit' element={<Edit/>}/>
-</Routes>
-</BrowserRouter>
+// import React, { Component } from "react";
+// import CBCPropEx from "./propsex/CBCPropEx";
+// import FBCPropsEx from "./propsex/FBCPropsEx";
 
-)
-}
-export default App
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* <CBCPropEx
+//           username="Subhapreet Patro"
+//           age={20}
+//           hobbies={["Video Games", "Playing chess"]}
+//           address={{ city: "Hyderabad", area: "Narsingi" }}
+//           sendFun={function () {
+//             alert("hi");
+//           }}
+//         /> */}
+//         <FBCPropsEx
+//           username="Rohan"
+//           isMarried={true}
+//           hobbies={["Playing video games", "Playing chess", "Exploring Tech"]}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// !props children
+// import React from "react";
+// import PropChildEx from "./propsex/PropChildEx";
+// import SubChild from "./propsex/SubChild";
+// import Child1 from "./propsex/Child1";
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <PropChildEx num={10}>
+//         <h1>This is data coming from props children</h1>
+//         <SubChild />
+//       </PropChildEx> */}
+//       <Child1 university="MRU" />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// !State example
+// import React from "react";
+// import CBCStateEx from "./stateexamples/CBCStateEx";
+// import FBCStateEx from "./stateexamples/FBCStateEx";
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <CBCStateEx /> */}
+//       <FBCStateEx />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+// import FunctionalComponent from "./components/FunctionalComponent";
+// import ClassComponent from "./components/ClassComponent";
+// import "./components/styles.css";
+// import styles from "./myStyles.module.css";
+// import ClassComponent2 from "./components/ClassComponent2";
+// import MyForm from "./components/MyForm";
+// import UseEffectEx from "./hookexamples/UseEffectEx";
+// import RefExamples from "./hookexamples/RefExamples";
+// import RefExample2 from "./hookexamples/RefExample2";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./crudcomponents/Home";
+import Create from "./crudcomponents/Create";
+import Edit from "./crudcomponents/Edit";
+import { ToastContainer } from "react-toastify";
+const App = () => {
+  return (
+    // <div>
+    //   App
+    //   {/* <h1 className="heading">This is a styling class</h1>
+    //   <h2 className={styles.error}>ERROR 404!</h2> */}
+    //   {/* <FunctionalComponent clgName="MRUH" city="Hyderabad" apply={false}>
+    //     <h1>Hello,Good afternoon</h1>
+    //     <p>Success</p>
+    //   </FunctionalComponent> */}
+    //   {/* <ClassComponent /> */}
+    //   {/* <ClassComponent2 /> */}
+    //   {/* <MyForm /> */}
+    //   {/* <UseEffectEx /> */}
+    //   {/* <RefExamples /> */}
+    //   {/* <RefExample2 /> */}
+    // </div>
+
+    <BrowserRouter>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/edit/:userId" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
